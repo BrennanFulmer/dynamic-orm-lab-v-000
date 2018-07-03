d7e389f773272df2595e70a30b645e25be7c55a7
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-03 03:59:17 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,7 +56,7 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute_hash)
-    attribute = attribute_hash.keys[0].to_s
+    attribute = attribute_hash.keys[0]
     value = attribute_hash.values[0]
     
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute} = '#{value}'"
